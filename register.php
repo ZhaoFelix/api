51f3739a5ad97ce05f64c697957ae5bcc37156b1
Felix <15021179915@163.com>
2018-06-27 22:58:51 +0800

    注册

---
+++ register.php
@@ -0,0 +1,44 @@
+<?php
+include_once 'include/template.php';
+
+$name = get("name");  //用户名
+$pwd = get("pwd");// 密码
+if(!isset($name) || !isset($pwd)){
+    $arr = [
+        "code" => "0",
+        "result" => "用户名或密码不存在"
+    ];
+    echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+            
+}
+else {
+    $id = getRowData("select UserId from tempDB.UserList where UserName='".$name."' and IsDeleted=0");
+    if (isset($id)){
+      $arr = [
+        "code" => "1",
+        "result" => "用户名已存在",
+    ];
+        
+     echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+    }
+    else {
+        $result = query("insert into tempDB.UserList values('".$name."','".$pwd."',now())");
+        if(isset($result)){
+            $arr = [
+                 "code" => "2",
+                "result" => "添加成功"
+            ];
+        }
+        else {
+            $arr = [
+                 "code" => "3",
+                "result" => "添加失败"
+            ];
+        }
+         
+   echo json_encode($arr,JSON_UNESCAPED_UNICODE);
+   }
+}
+
+
+
