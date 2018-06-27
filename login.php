aa5cf7307cf8a7feae5d977ba347c57088759f56
Felix <15021179915@163.com>
2018-06-27 21:33:19 +0800

    登录接口

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+include_once 'include/template.php';
+
+$name = get("name");  //用户名
+$pwd = get("pwd");// 密码
+
+if(!isset($name) || !isset($pwd)){
+    $arr = [
+        "code" => "0",
+        "result" => "用户名或密码不存在"
+    ];
+    echo json_endoce($arr);
+            
+}
+else {
+    $id = getRowData("select UserId from TempDB.UserList where UserName='".$name."' and Password='".$pwd."' and IsDeleed=0");
+    if (isset($id)){
+      $arr = [
+        "code" => "1",
+        "result" => "登录成功"
+    ];
+        
+      echo json_encode($arr);
+    }
+    else {
+         $arr = [
+        "code" => "1",
+        "result" => "用户名或密码不正确"
+    ];
+    json_encode($arr);
+    }
+}
+
+?>
