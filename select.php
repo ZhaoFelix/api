f471ff01ad493cb717c951c81caaab7fa8331eaa
Felix <15021179915@163.com>
2018-07-02 19:07:29 +0800

    te

---
+++ select.php
@@ -0,0 +1,32 @@
+<?php
+include_once 'include/template.php';
+
+$id = get("id");  //用户名
+
+if (!isset($id))
+{
+     $arr = [
+        "code" => "0",
+        "result" => "用户名或密码不存在"
+    ];
+    echo json_encode($arr,JSON_UNESCAPED_UNICODE);  
+}
+else {
+    $data = getData("select ListContent  from tempDB.UserChickList where UserId='".$id."' and IsDeleted=0");
+   if(count($data)!=0){
+        $arr = [
+        "code" => "1",
+        "result" => "成功",
+         "userId" => $data
+    ];
+     echo json_encode($arr,JSON_UNESCAPED_UNICODE); 
+     
+   }
+   else {
+      $arr = [
+        "code" => "2",
+        "result" => "没有数据"
+    ]; 
+     echo json_encode($arr,JSON_UNESCAPED_UNICODE);   
+   }
+}
