017793f90710f7ea97748bdf1af93513aa62101e
Felix <15021179915@163.com>
2018-07-09 11:43:08 +0800

    查询全部的数据

---
+++ all.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'include/template.php';
+
+
+$data = getData("select * from tempDB.UserChickList order by CreateTime");
+if(count($data)>0){
+    $re = [
+        "code"=>"1",
+        "result"=>$data
+    ];
+    echo json_decode($re,JSON_UNESCAPED_UNICODE);//设置编码格式，确保中文不会乱码
+}
+else {
+    $re = [
+        "code"=>"0",
+        "result"=>"没有数据"
+    ];
+     echo json_encode($re,JSON_UNESCAPED_UNICODE);    
+}
